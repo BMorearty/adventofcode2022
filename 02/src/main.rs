da55667b4aaed19548909240ac0df1e23684e9ff
Brian Morearty <brian@morearty.org>
2022-12-02 12:22:23 -0800

    Factor out common code.

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -6,10 +6,10 @@ use std::{fs, io};
 const FILENAME: &str = "data/input";
 
 fn main() {
-    let score = part1_calc_score();
+    let score = calc_score(choices1);
     println!("Part 1: {}", score);
 
-    let score = part2_calc_score();
+    let score = calc_score(choices2);
     println!("Part 2: {}", score);
 }
 
@@ -31,7 +31,7 @@ fn win_lose_draw_score(theirs: &str, mine: &str) -> u32 {
     return scores[&combined];
 }
 
-fn part1_calc_score() -> u32 {
+fn calc_score(mine_func: fn(&str) -> (&str, &str)) -> u32 {
     let values: HashMap<String, u32> = map! {"X": 1u32, "Y": 2u32, "Z": 3u32};
     let file = fs::File::open(FILENAME).expect("No such file");
     let mut buf = io::BufReader::new(file);
@@ -42,8 +42,7 @@ fn part1_calc_score() -> u32 {
         if bytes == 0 {
             break;
         }
-        let theirs = &line[0..1];
-        let mine = &line[2..3];
+        let (theirs, mine) = mine_func(&line);
         score += win_lose_draw_score(theirs, mine) + values[mine];
         line = "".to_string();
     }
@@ -51,34 +50,26 @@ fn part1_calc_score() -> u32 {
     score
 }
 
-fn part2_calc_score() -> u32 {
-    let values: HashMap<String, u32> = map! {"X": 1u32, "Y": 2u32, "Z": 3u32};
-    let file = fs::File::open(FILENAME).expect("No such file");
-    let mut buf = io::BufReader::new(file);
-    let mut line: String = "".to_string();
-    let mut score: u32 = 0;
+fn choices1(line: &str) -> (&str, &str) {
+    let theirs = &line[0..1];
+    let mine = &line[2..3];
+    (theirs, mine)
+}
 
-    while let Ok(bytes) = buf.read_line(&mut line) {
-        if bytes == 0 {
-            break;
-        }
-        let theirs = &line[0..1];
-        let win_lose_or_draw = &line[2..3];
-        let mine = match (theirs, win_lose_or_draw) {
-            ("A", "X") => "Z",
-            ("A", "Y") => "X",
-            ("A", "Z") => "Y",
-            ("B", "X") => "X",
-            ("B", "Y") => "Y",
-            ("B", "Z") => "Z",
-            ("C", "X") => "Y",
-            ("C", "Y") => "Z",
-            ("C", "Z") => "X",
-            _ => "",
-        };
-        score += win_lose_draw_score(theirs, mine) + values[mine];
-        line = "".to_string();
-    }
-
-    score
+fn choices2(line: &str) -> (&str, &str) {
+    let theirs = &line[0..1];
+    let win_lose_or_draw = &line[2..3];
+    let mine = match (theirs, win_lose_or_draw) {
+        ("A", "X") => "Z",
+        ("A", "Y") => "X",
+        ("A", "Z") => "Y",
+        ("B", "X") => "X",
+        ("B", "Y") => "Y",
+        ("B", "Z") => "Z",
+        ("C", "X") => "Y",
+        ("C", "Y") => "Z",
+        ("C", "Z") => "X",
+        _ => "",
+    };
+    (theirs, mine)
 }
