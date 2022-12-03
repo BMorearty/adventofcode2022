d0777e160e83b2a658c09daca1e37774fdc1cb58
Brian Morearty <brian@morearty.org>
2022-12-02 21:46:49 -0800

    template

---
+++ template/src/main.rs
@@ -0,0 +1,24 @@
+use std::io::{BufRead, Seek};
+use std::{env, fs, io};
+use std::fs::File;
+use std::io::SeekFrom::Start;
+
+fn main() {
+    let filename = get_filename();
+    let file = fs::File::open(filename).expect("No such file");
+    let mut buf = io::BufReader::new(file);
+    part1(&mut buf);
+    let _ = buf.seek(Start(0));
+    part2(&mut buf);
+}
+
+fn get_filename() -> String {
+    env::args()
+        .nth(1)
+        .expect("Specify an input file")
+        .to_string()
+}
+
+fn part1(buf: &mut io::BufReader<File>) {}
+
+fn part2(buf: &mut io::BufReader<File>) {}
