c68fcb87b669a8dd0b52d88ada2ce9ec97408c02
Brian Morearty <brian@morearty.org>
2022-12-08 10:45:04 -0800

    Switch to promise-based readline.

--- template/src/main-readline.mjs
+++ template/src/main-readline.mjs
@@ -2,26 +2,13 @@ import process from 'process';
 import readline from 'readline';
 import fs from 'fs';
 
-function readLines(line, close) {
+async function run() {
   const filename = process.argv[2];
   const stream = fs.createReadStream(filename, 'utf8');
   const rl = readline.createInterface(stream);
-  rl.on('line', line);
-  rl.on('close', close);
-}
-
-async function run() {
-  return new Promise(resolve => {
-    function line(line) {
-      
-    }
-
-    function close() {
-      resolve();
-    }
-
-    readLines(line, close);
-  });
+  for await (const line of rl) {
+    console.log(line);
+  }
 }
 
 await run();
