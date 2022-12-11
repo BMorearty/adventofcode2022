e681511caa02b436a6bb079337121430f88ec87f
Brian Morearty <brian@morearty.org>
2022-12-10 20:58:14 -0800

    chnage template

--- template/src/main.mjs
+++ template/src/main.mjs
@@ -1,24 +1,14 @@
 import process from 'process';
+import readline from 'readline';
 import fs from 'fs';
-import _ from 'lodash';
 
-const filename = process.argv[2];
-const contents = fs.readFileSync(filename, 'utf8');
-let lines = contents.split('\n').filter(line => line.length > 0);
-
-part1(lines);
-part2(lines);
-
-function part1(lines) {
-  let result = 0;
-  for (const line of lines) {
+async function run() {
+  const filename = process.argv[2];
+  const stream = fs.createReadStream(filename, 'utf8');
+  const rl = readline.createInterface(stream);
+  for await (const line of rl) {
+    console.log(line);
   }
-  console.log(`Part 1: ${result}`);
 }
 
-function part2(lines) {
-  let result = 0;
-  for (const line of lines) {
-  }
-  console.log(`Part 2: ${result}`);
-}
+await run();
