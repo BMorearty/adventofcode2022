0c6aa42c88ce46e08b5bd30c19b4a648d07a5e61
Brian Morearty <brian@morearty.org>
2022-12-11 18:19:53 -0800

    change template to have a parsing regex ready to go

--- template/src/main.mjs
+++ template/src/main.mjs
@@ -1,13 +1,24 @@
 import process from 'process';
-import readline from 'readline';
 import fs from 'fs';
+
+function process(s) {
+  const r = /([-\d, ]+).*?\s.*?/i;
+  const m = r.exec(s);
+  return {
+    numgroup: m[1].split(/-, /).map(n => +n),
+    singlenum: +m[2],
+  }
+}
 
 async function run() {
   const filename = process.argv[2];
-  const stream = fs.createReadStream(filename, 'utf8');
-  const rl = readline.createInterface(stream);
-  for await (const line of rl) {
-    console.log(line);
+  const contents = fs.readFileSync(filename, 'utf8');
+
+  // If items are one per line, change \n\n to \n
+  const items = contents.split('\n\n').map(item => process(item));
+
+  for (const item of items) {
+    
   }
 }
 
